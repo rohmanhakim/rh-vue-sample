c247f3febddbb615b1b0c345c701cb655f2f4592
rohmanhakim <rohmanhakim@live.com>
2016-05-24 14:15:45 +0700

    added files for new tutorial : https://scotch.io/tutorials/build-an-app-with-vue-js-a-lightweight-alternative-to-angularjs

---
+++ app.js
@@ -0,0 +1,14 @@
+new Vue({
+    // we want to target the div with an id of 'events'
+    el: '#events',
+
+    //here we can register any values or collections that hold data
+    // for the application
+    data: {},
+
+    //anything within the ready function will run when the application loads
+    ready: function() {},
+
+    //methods we want to use in our application are registered here
+    methods: {}
+});
